6f0ab7ca804b67391a52c5b26a2c5939acc97c6a
Janesh Kodikara <janesh.kodikara@gmail.com>
2024-12-06 20:36:42 +0530

    Integrate java faker into test suite

--- src/test/java/com/pragmatic/selenium/sauce/CheckoutFlowTest.java
+++ src/test/java/com/pragmatic/selenium/sauce/CheckoutFlowTest.java
@@ -1,5 +1,6 @@
 package com.pragmatic.selenium.sauce;
 
+import com.github.javafaker.Faker;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -33,15 +34,16 @@ public class CheckoutFlowTest {
 
     @Test
     public void testLoginWithValidCredentials() {
+        Faker faker = new Faker();
         webDriver.findElement(By.id("user-name")).sendKeys("standard_user");
         webDriver.findElement(By.id("password")).sendKeys("secret_sauce");
         webDriver.findElement(By.id("login-button")).click();
         webDriver.findElement(By.id("add-to-cart-sauce-labs-backpack")).click();
         webDriver.findElement(By.cssSelector(".shopping_cart_link")).click();
         webDriver.findElement(By.id("checkout")).click();
-        webDriver.findElement(By.id("first-name")).sendKeys("Janesh");
-        webDriver.findElement(By.id("last-name")).sendKeys("Kodikara");
-        webDriver.findElement(By.id("postal-code")).sendKeys("11850");
+        webDriver.findElement(By.id("first-name")).sendKeys(faker.name().firstName());
+        webDriver.findElement(By.id("last-name")).sendKeys(faker.name().lastName());
+        webDriver.findElement(By.id("postal-code")).sendKeys(faker.address().zipCode());
         webDriver.findElement(By.id("continue")).click();
         webDriver.findElement(By.id("finish")).click();
         Assert.assertEquals(webDriver.findElement(By.cssSelector("h2.complete-header")).getText(), "Thank you for your order!");
