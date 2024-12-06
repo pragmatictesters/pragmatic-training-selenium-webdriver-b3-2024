f507653b36a084af6ee85df245e56f0be91a0f90
Janesh Kodikara <janesh.kodikara@gmail.com>
2024-12-06 11:38:43 +0530

    Initial commit: Selenium training content

---
+++ src/test/java/com/pragmatic/selenium/HelloSeleniumTest.java
@@ -0,0 +1,26 @@
+package com.pragmatic.selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class HelloSeleniumTest {
+
+
+
+    @Test
+    public void  testSauceLogin(){
+        WebDriver webDriver = new ChromeDriver();
+        webDriver.get("https://www.saucedemo.com");
+        webDriver.findElement(By.id("user-name")).sendKeys("standard_user");
+        webDriver.findElement(By.id("password")).sendKeys("secret_sauce");
+        webDriver.findElement(By.id("login-button")).click();
+        Assert.assertEquals(webDriver.findElement(By.id("span.title")).getText(), "Products");
+        webDriver.close();
+
+
+    }
+
+}
