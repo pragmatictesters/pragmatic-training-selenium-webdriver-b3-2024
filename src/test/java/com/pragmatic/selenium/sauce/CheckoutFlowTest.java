4dd1ec3747d6654d5b2bbf9de0858d7c42613725
Janesh Kodikara <janesh.kodikara@gmail.com>
2024-12-14 14:41:26 +0530

    Incorporate dateFaker instead of JavaFaker into the tests

--- src/test/java/com/pragmatic/selenium/sauce/CheckoutFlowTest.java
+++ src/test/java/com/pragmatic/selenium/sauce/CheckoutFlowTest.java
@@ -1,6 +1,7 @@
 package com.pragmatic.selenium.sauce;
 
-import com.github.javafaker.Faker;
+
+import net.datafaker.Faker;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -9,15 +10,10 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class CheckoutFlowTest {
+public class CheckoutFlowTest  {
 
-    private final String BASE_URL;
+    private final String BASE_URL = "http://saucedemo.com";
     private WebDriver webDriver;
-
-    public CheckoutFlowTest() {
-        BASE_URL = "http://saucedemo.com";
-    }
-
 
     @BeforeMethod
     public void beforeMethod() {
@@ -34,16 +30,21 @@ public class CheckoutFlowTest {
 
     @Test
     public void testLoginWithValidCredentials() {
+
         Faker faker = new Faker();
+
+        String firstName = faker.name().firstName(); // Emory
+        String lastName = faker.name().lastName();
+
         webDriver.findElement(By.id("user-name")).sendKeys("standard_user");
         webDriver.findElement(By.id("password")).sendKeys("secret_sauce");
         webDriver.findElement(By.id("login-button")).click();
         webDriver.findElement(By.id("add-to-cart-sauce-labs-backpack")).click();
         webDriver.findElement(By.cssSelector(".shopping_cart_link")).click();
         webDriver.findElement(By.id("checkout")).click();
-        webDriver.findElement(By.id("first-name")).sendKeys(faker.name().firstName());
-        webDriver.findElement(By.id("last-name")).sendKeys(faker.name().lastName());
-        webDriver.findElement(By.id("postal-code")).sendKeys(faker.address().zipCode());
+        webDriver.findElement(By.id("first-name")).sendKeys(firstName);
+        webDriver.findElement(By.id("last-name")).sendKeys(lastName);
+        webDriver.findElement(By.id("postal-code")).sendKeys("12345");
         webDriver.findElement(By.id("continue")).click();
         webDriver.findElement(By.id("finish")).click();
         Assert.assertEquals(webDriver.findElement(By.cssSelector("h2.complete-header")).getText(), "Thank you for your order!");
