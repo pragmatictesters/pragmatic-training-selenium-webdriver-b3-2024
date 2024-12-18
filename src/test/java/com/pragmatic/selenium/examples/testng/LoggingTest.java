5c56a9c92aa415c0d40912d5e31cef9f5e2a958c
Janesh Kodikara <janesh.kodikara@gmail.com>
2024-12-18 11:16:39 +0530

    Add example to demonstrate TestNG logging

---
+++ src/test/java/com/pragmatic/selenium/examples/testng/LoggingTest.java
@@ -0,0 +1,15 @@
+package com.pragmatic.selenium.examples.testng;
+
+import org.testng.annotations.Test;
+import org.testng.log4testng.Logger;
+
+public class LoggingTest {
+
+    private static final Logger LOGGER = Logger.getLogger(LoggingTest.class);
+
+
+    @Test
+    public void testLogging() {
+        LOGGER.debug("entering myMethod()");
+    }
+}
