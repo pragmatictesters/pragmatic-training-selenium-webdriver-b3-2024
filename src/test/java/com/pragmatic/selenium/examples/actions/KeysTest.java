4000e0c4fff5eaf9bac520a3eca9f5376c1029d3
Janesh Kodikara <janesh.kodikara@gmail.com>
2025-02-02 20:10:53 +0530

    Fix issue in typing capital letters with the Actions

--- src/test/java/com/pragmatic/selenium/examples/actions/KeysTest.java
+++ src/test/java/com/pragmatic/selenium/examples/actions/KeysTest.java
@@ -1,9 +1,6 @@
 package com.pragmatic.selenium.examples.actions;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
@@ -16,29 +13,56 @@ public class KeysTest {
     WebDriver webDriver;
 
     @BeforeMethod
-    public void beforeMethod(){
-         webDriver = new ChromeDriver();
+    public void beforeMethod() {
+        webDriver = new ChromeDriver();
     }
 
     @AfterMethod
-    public void afterMethod(){
+    public void afterMethod() {
         webDriver.quit();
     }
 
     @Test
     public void keyUp() {
 
-        webDriver.get("https://www.selenium.dev/selenium/web/single_text_input.html");
-        WebElement textField = webDriver.findElement(By.id("textInput"));
-//        textField.sendKeys("Ab");
+        webDriver.get("https://www.saucedemo.com/");
+        WebElement textField = webDriver.findElement(By.id("user-name"));
 
-        new Actions(webDriver)
-                .keyDown(Keys.SHIFT)
-                .sendKeys("P")
+
+        Actions actions = new Actions(webDriver);
+        actions.keyDown(Keys.SHIFT)
+                .sendKeys(textField, "p")
                 .keyUp(Keys.SHIFT)
-                .sendKeys("ragmatic")
                 .perform();
 
-        Assert.assertEquals(textField.getDomProperty("value"),"Pragmatic");
+        actions.sendKeys(textField, "ragmatic")
+                .perform();
+
+        Assert.assertEquals(textField.getDomProperty("value"), "Pragmatic");
     }
+
+    @Test
+    public void testCopyAndPaste() {
+        webDriver.get("https://www.selenium.dev/selenium/web/web-form.html");
+        WebElement textField = webDriver.findElement(By.id("my-text-id"));
+        Keys controlKey = getControlKey();
+
+        new Actions(webDriver)
+                .sendKeys(textField, "Selenium!")
+                .sendKeys(textField, Keys.ARROW_LEFT)
+                .keyDown(Keys.SHIFT)
+                .sendKeys(Keys.ARROW_UP)
+                .keyUp(Keys.SHIFT)
+                .keyDown(controlKey)
+                .sendKeys("xvvv")
+                .keyUp(controlKey)
+                .perform();
+        Assert.assertEquals(textField.getDomProperty("value"), "SeleniumSeleniumSelenium!");
+    }
+
+    private static Keys getControlKey() {
+        return Platform.getCurrent().is(Platform.MAC) ? Keys.COMMAND : Keys.CONTROL;
+    }
+
+
 }
